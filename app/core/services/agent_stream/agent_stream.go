6d7ab41f1ac3885808745fd42feb70c444cab9ad
orbit-w <wbwwxm@gmail.com>
2025-03-23 21:53:44 +0800

    fix:修复构建失败问题

--- app/core/services/agent_stream/agent_stream.go
+++ app/core/services/agent_stream/agent_stream.go
@@ -59,7 +59,7 @@ var streamHandle = func(stream mux.IServerConn) error {
 		}
 
 		for _, msg := range msgList {
-			requestHandler(msg.Data, msg.Seq, msg.Pid)
+			requestHandler(session, msg.Data, msg.Seq, msg.Pid)
 		}
 	}
 	return nil
