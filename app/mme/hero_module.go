983c2e6700b7c0a69ee557235f4061720047e4a0
orbit-w <wbwwxm@gmail.com>
2025-10-25 21:55:00 +0800

    refactor(hero_module): replace DirtyTracker with DirtyFlag and update HeroModule to use generics for better type handling

--- app/mme/hero_module.go
+++ app/mme/hero_module.go
@@ -1,8 +1,8 @@
 package mme
 
 import (
-	"gitee.com/orbit-w/meteor/bases/dirty/dirty_tracker"
 	"gitee.com/orbit-w/orbit/app/proto/mme"
+	dirtyflag "gitee.com/orbit-w/orbit/lib/base/dirty_flag"
 )
 
 // Dirty bits for Module fields
@@ -12,39 +12,39 @@ const (
 	HeroModuleDirtyLevelUpBit int64 = 1 << 2
 )
 
-type HeroModule struct {
+type HeroModule[FactsAccessorKey comparable] struct {
 	mme *mme.HeroModule
-	dirty_tracker.DirtyTracker
+	dirtyflag.IDirtyFlag[FactsAccessorKey]
 
-	Base    *HeroMechanism[int32]
-	LevelUp *LevelUpMechanism[int32]
+	Base    *HeroMechanism[any]
+	LevelUp *LevelUpMechanism[any]
 }
 
-func NewHeroModule(pt *mme.HeroModule) *HeroModule {
+func NewHeroModule[FactsAccessorKey comparable](pt *mme.HeroModule) *HeroModule[FactsAccessorKey] {
 	if pt == nil {
 		panic("pt is nil")
 	}
-	m := &HeroModule{
-		mme: pt,
+	m := &HeroModule[FactsAccessorKey]{
+		mme:        pt,
+		IDirtyFlag: dirtyflag.NewDirtyFlag[FactsAccessorKey](),
 	}
-	// 非
-	m.Base = NewHeroMechanism[int32](m.mme.Base)
-	m.Base.Link(&m.DirtyTracker, HeroModuleDirtyBaseBit)
+	// Mechanism obj 需要Link到DirtyTracker
+	// xmap类型，
+	// if Value 是Mechanism，则需要通过LinkFactsAccessor到Father和xmap的Key
 
-	m.LevelUp = NewLevelUpMechanism[int32](m.mme.LevelUp)
-	m.LevelUp.Link(&m.DirtyTracker, HeroModuleDirtyLevelUpBit)
+	m.Base = NewHeroMechanism[any](m.mme.Base)
+	m.Base.Link(m.GetDirtyTracker(), HeroModuleDirtyBaseBit)
+
+	m.LevelUp = NewLevelUpMechanism[any](m.mme.LevelUp)
+	m.LevelUp.Link(m.GetDirtyTracker(), HeroModuleDirtyLevelUpBit)
 	return m
 }
 
-func (m *HeroModule) Name() string {
+func (m *HeroModule[FactsAccessorKey]) Name() string {
 	return "HeroModule"
 }
 
-func (m *HeroModule) Link(parent *dirty_tracker.DirtyTracker, parentBit int64) {
-	m.DirtyTracker.Link(parent, parentBit)
-}
-
-func (m *HeroModule) DeepCopy(co *mme.HeroModule) {
+func (m *HeroModule[FactsAccessorKey]) DeepCopy(co *mme.HeroModule) {
 	if m == nil || co == nil {
 		return
 	}
