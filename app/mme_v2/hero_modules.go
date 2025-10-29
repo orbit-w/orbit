b26698a0b017c9b987c02f5e8c0a517f015cd960
wangbowei <wangbowei@t4f-mba-2509200.nibirutech.com>
2025-10-29 12:27:17 +0800

    feat(hero_module): add HeroModule and HeroModuleWrapper with dirty flag management, MongoDB update support, and protobuf conversion methods

---
+++ app/mme_v2/hero_modules.go
@@ -0,0 +1,241 @@
+package mme
+
+import (
+	"gitee.com/orbit-w/orbit/app/proto/mme"
+	dirtyflag "gitee.com/orbit-w/orbit/lib/base/dirty_flag"
+	fieldmeta "gitee.com/orbit-w/orbit/lib/base/field_meta"
+	"gitee.com/orbit-w/orbit/lib/module/db/mgo_builder"
+	mmeutils "gitee.com/orbit-w/orbit/lib/module/mme_utils"
+	"google.golang.org/protobuf/proto"
+)
+
+const (
+	HeroModuleFieldIndexBase = uint8(0)
+	HeroModuleFieldIndexLevelUp
+)
+
+// Dirty bits for HeroModule fields
+const (
+	HeroModuleDirtyBaseBit    int64 = 1 << HeroModuleFieldIndexBase
+	HeroModuleDirtyLevelUpBit int64 = 1 << HeroModuleFieldIndexLevelUp
+)
+
+type HeroModule struct {
+	Base    *HeroMechanism    `bson:"base"`
+	LevelUp *LevelUpMechanism `bson:"level_up"`
+}
+
+type HeroModuleWrapper struct {
+	data *HeroModule
+	dirtyflag.IDirtyFlag
+	fieldMetas *fieldmeta.FieldMetas
+
+	BaseWrapper    *HeroMechanismWrapper
+	LevelUpWrapper *LevelUpMechanismWrapper
+}
+
+func NewHeroModuleWrapper(data *HeroModule) *HeroModuleWrapper {
+	if data == nil {
+		panic("data is nil")
+	}
+	hm := &HeroModuleWrapper{
+		data:       data,
+		IDirtyFlag: dirtyflag.NewDirtyFlag(),
+		fieldMetas: fieldmeta.NewFieldMetas(),
+	}
+
+	// 初始化嵌套的 Mechanism Wrapper
+	if data.Base != nil {
+		hm.BaseWrapper = NewHeroMechanismWrapper(data.Base)
+		hm.BaseWrapper.Link(hm.GetDirtyTracker(), HeroModuleDirtyBaseBit)
+	}
+
+	if data.LevelUp != nil {
+		hm.LevelUpWrapper = NewLevelUpMechanismWrapper(data.LevelUp)
+		hm.LevelUpWrapper.Link(hm.GetDirtyTracker(), HeroModuleDirtyLevelUpBit)
+	}
+
+	return hm
+}
+
+func (m *HeroModuleWrapper) InitFieldContext() {
+	m.fieldMetas.SetFieldType(HeroModuleFieldIndexBase, fieldmeta.FieldTypeSync)
+	m.fieldMetas.SetFieldType(HeroModuleFieldIndexLevelUp, fieldmeta.FieldTypeSync)
+
+	// 初始化嵌套 Mechanism 的字段上下文
+	if m.BaseWrapper != nil {
+		m.BaseWrapper.InitFieldContext()
+	}
+	if m.LevelUpWrapper != nil {
+		m.LevelUpWrapper.InitFieldContext()
+	}
+}
+
+// 模块唯一名称
+func (m *HeroModuleWrapper) Name() string {
+	return "HeroModule"
+}
+
+func (m *HeroModuleWrapper) GetBase() *HeroMechanismWrapper {
+	return m.BaseWrapper
+}
+
+func (m *HeroModuleWrapper) GetLevelUp() *LevelUpMechanismWrapper {
+	return m.LevelUpWrapper
+}
+
+// ClearAllDirtyFlags 清除所有脏标记位
+func (m *HeroModuleWrapper) ClearAllDirtyFlags() {
+	m.ClearAllDirty()
+
+	// 清除嵌套 Mechanism 的脏标记
+	if m.BaseWrapper != nil {
+		m.BaseWrapper.ClearAllDirtyFlags()
+	}
+	if m.LevelUpWrapper != nil {
+		m.LevelUpWrapper.ClearAllDirtyFlags()
+	}
+}
+
+// BuildMongoUpdate 构建MongoDB更新操作
+func (m *HeroModuleWrapper) BuildMongoUpdate(builder *mgo_builder.MongoUpdateBuilder, prefix string) {
+	if m == nil {
+		return
+	}
+
+	// 构建嵌套字段路径
+	if m.BaseWrapper != nil && m.IsDirty(HeroModuleDirtyBaseBit) {
+		basePrefix := prefix
+		if prefix != "" {
+			basePrefix = prefix + ".base"
+		} else {
+			basePrefix = "base"
+		}
+		m.BaseWrapper.BuildMongoUpdate(builder, basePrefix)
+	}
+
+	if m.LevelUpWrapper != nil && m.IsDirty(HeroModuleDirtyLevelUpBit) {
+		levelUpPrefix := prefix
+		if prefix != "" {
+			levelUpPrefix = prefix + ".level_up"
+		} else {
+			levelUpPrefix = "level_up"
+		}
+		m.LevelUpWrapper.BuildMongoUpdate(builder, levelUpPrefix)
+	}
+}
+
+func (m *HeroModuleWrapper) DeepCopy(copy *HeroModule) {
+	if m == nil || m.data == nil || copy == nil {
+		return
+	}
+
+	// 拷贝嵌套的 Mechanism 对象
+	if m.data.Base != nil {
+		if copy.Base == nil {
+			copy.Base = &HeroMechanism{}
+		}
+		m.BaseWrapper.DeepCopy(copy.Base)
+	}
+
+	if m.data.LevelUp != nil {
+		if copy.LevelUp == nil {
+			copy.LevelUp = &LevelUpMechanism{}
+		}
+		m.LevelUpWrapper.DeepCopy(copy.LevelUp)
+	}
+}
+
+// MatchesAll 判断字段是否匹配所有类型标记
+func (m *HeroModuleWrapper) MatchesAll(fieldID uint8, fieldTypes ...fieldmeta.FieldType) bool {
+	return m.fieldMetas.MatchesAll(fieldID, fieldTypes...)
+}
+
+// ToProto 将 HeroModule 数据转换为完整的 protobuf 结构体
+func (m *HeroModuleWrapper) ToProto() *mme.HeroModule {
+	if m == nil || m.data == nil {
+		return nil
+	}
+
+	pb := &mme.HeroModule{}
+
+	// 转换嵌套的 Mechanism 对象
+	if m.BaseWrapper != nil {
+		pb.Base = m.BaseWrapper.ToProto()
+	}
+
+	if m.LevelUpWrapper != nil {
+		pb.LevelUp = m.LevelUpWrapper.ToProto()
+	}
+
+	return pb
+}
+
+// FromProto 从 protobuf 结构体加载数据到 HeroModule
+func (m *HeroModuleWrapper) FromProto(pb *mme.HeroModule) {
+	if m == nil || m.data == nil || pb == nil {
+		return
+	}
+
+	// 加载 Base 数据
+	if pb.Base != nil {
+		if m.data.Base == nil {
+			m.data.Base = &HeroMechanism{}
+			m.BaseWrapper = NewHeroMechanismWrapper(m.data.Base)
+			m.BaseWrapper.Link(m.GetDirtyTracker(), HeroModuleDirtyBaseBit)
+		}
+		m.BaseWrapper.FromProto(pb.Base)
+	}
+
+	// 加载 LevelUp 数据
+	if pb.LevelUp != nil {
+		if m.data.LevelUp == nil {
+			m.data.LevelUp = &LevelUpMechanism{}
+			m.LevelUpWrapper = NewLevelUpMechanismWrapper(m.data.LevelUp)
+			m.LevelUpWrapper.Link(m.GetDirtyTracker(), HeroModuleDirtyLevelUpBit)
+		}
+		m.LevelUpWrapper.FromProto(pb.LevelUp)
+	}
+}
+
+// ToIncrementalProto 根据脏标记位构建增量数据的 protoMessage
+// 只返回标记为脏的字段数据，用于增量同步
+func (m *HeroModuleWrapper) ToIncrementalProto(ctx mmeutils.SyncContext) proto.Message {
+	if m == nil {
+		return nil
+	}
+
+	// 如果没有脏标记，返回 nil
+	if !m.HasAnyDirty() {
+		return nil
+	}
+
+	incremental := &mme.HeroModule{}
+
+	// 根据脏标记位设置对应的嵌套对象
+	if mmeutils.FieldCanBeIncrementalSynced(m, HeroModuleDirtyBaseBit, HeroModuleFieldIndexBase, ctx) {
+		if m.BaseWrapper != nil {
+			pb := m.BaseWrapper.ToIncrementalProto(ctx)
+			if pb != nil {
+				v, ok := pb.(*mme.HeroMechanism)
+				if ok {
+					incremental.Base = v
+				}
+			}
+		}
+	}
+
+	if mmeutils.FieldCanBeIncrementalSynced(m, HeroModuleDirtyLevelUpBit, HeroModuleFieldIndexLevelUp, ctx) {
+		if m.LevelUpWrapper != nil {
+			pb := m.LevelUpWrapper.ToIncrementalProto(ctx)
+			if pb != nil {
+				v, ok := pb.(*mme.LevelUpMechanism)
+				if ok {
+					incremental.LevelUp = v
+				}
+			}
+		}
+	}
+
+	return incremental
+}
