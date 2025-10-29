7822c45bc6b7611feea49c610ea31979c03e87a4
wangbowei <wangbowei@t4f-mba-2509200.nibirutech.com>
2025-10-29 12:15:37 +0800

    feat(level_up_mechanism): implement LevelUpMechanismWrapper with dirty flag management, MongoDB update builder integration, and protobuf conversion methods

---
+++ app/mme_v2/level_up_mechanisms.go
@@ -0,0 +1,219 @@
+package mme
+
+import (
+	"strings"
+
+	"gitee.com/orbit-w/orbit/app/proto/mme"
+	dirtyflag "gitee.com/orbit-w/orbit/lib/base/dirty_flag"
+	fieldmeta "gitee.com/orbit-w/orbit/lib/base/field_meta"
+	"gitee.com/orbit-w/orbit/lib/module/db/mgo_builder"
+	mmeutils "gitee.com/orbit-w/orbit/lib/module/mme_utils"
+	"google.golang.org/protobuf/proto"
+)
+
+const (
+	LevelUpMechanismFieldIndexCurLevel = uint8(0)
+	LevelUpMechanismFieldIndexCurExp
+	LevelUpMechanismFieldIndexConfId
+)
+
+// Dirty bits for LevelUpMechanism fields
+const (
+	LevelUpMechanismDirtyCurLevelBit int64 = 1 << LevelUpMechanismFieldIndexCurLevel
+	LevelUpMechanismDirtyCurExpBit   int64 = 1 << LevelUpMechanismFieldIndexCurExp
+	LevelUpMechanismDirtyConfIdBit   int64 = 1 << LevelUpMechanismFieldIndexConfId
+)
+
+type LevelUpMechanism struct {
+	CurLevel int32 `bson:"cur_level"`
+	CurExp   int32 `bson:"cur_exp"`
+	ConfId   int32 `bson:"conf_id"`
+}
+
+type LevelUpMechanismWrapper struct {
+	data *LevelUpMechanism
+	dirtyflag.IDirtyFlag
+	fieldMetas *fieldmeta.FieldMetas
+}
+
+func NewLevelUpMechanismWrapper(data *LevelUpMechanism) *LevelUpMechanismWrapper {
+	if data == nil {
+		panic("data is nil")
+	}
+	lm := &LevelUpMechanismWrapper{
+		data:       data,
+		IDirtyFlag: dirtyflag.NewDirtyFlag(),
+		fieldMetas: fieldmeta.NewFieldMetas(),
+	}
+
+	return lm
+}
+
+func (m *LevelUpMechanismWrapper) InitFieldContext() {
+	m.fieldMetas.SetFieldType(LevelUpMechanismFieldIndexCurLevel, fieldmeta.FieldTypeSync)
+	m.fieldMetas.SetFieldType(LevelUpMechanismFieldIndexCurExp, fieldmeta.FieldTypeSync)
+	m.fieldMetas.SetFieldType(LevelUpMechanismFieldIndexConfId, fieldmeta.FieldTypeSync)
+}
+
+// 机制唯一名称
+func (m *LevelUpMechanismWrapper) Name() string {
+	return "LevelUpMechanism"
+}
+
+func (m *LevelUpMechanismWrapper) GetCurLevel() int32 {
+	return m.data.CurLevel
+}
+
+func (m *LevelUpMechanismWrapper) GetCurExp() int32 {
+	return m.data.CurExp
+}
+
+func (m *LevelUpMechanismWrapper) GetConfId() int32 {
+	return m.data.ConfId
+}
+
+func (m *LevelUpMechanismWrapper) SetCurLevel(v int32) {
+	m.data.CurLevel = v
+	m.MarkDirty(LevelUpMechanismDirtyCurLevelBit)
+}
+
+func (m *LevelUpMechanismWrapper) SetCurExp(v int32) {
+	m.data.CurExp = v
+	m.MarkDirty(LevelUpMechanismDirtyCurExpBit)
+}
+
+func (m *LevelUpMechanismWrapper) SetConfId(v int32) {
+	m.data.ConfId = v
+	m.MarkDirty(LevelUpMechanismDirtyConfIdBit)
+}
+
+// ClearAllDirtyFlags 清除所有脏标记位
+func (m *LevelUpMechanismWrapper) ClearAllDirtyFlags() {
+	m.ClearAllDirty()
+}
+
+// BuildMongoUpdate 构建MongoDB更新操作
+func (m *LevelUpMechanismWrapper) BuildMongoUpdate(builder *mgo_builder.MongoUpdateBuilder, prefix string) {
+	if m == nil {
+		return
+	}
+
+	// 使用 strings.Builder 优化路径构建性能
+	var pathBuilder strings.Builder
+	hasPrefix := prefix != ""
+
+	// 预分配容量，减少内存重新分配
+	// 预估：prefix长度 + "." + 最长字段名("cur_level"=9)
+	if hasPrefix {
+		pathBuilder.Grow(len(prefix) + 10)
+	}
+
+	// 辅助函数：构建字段路径
+	buildPath := func(fieldName string) string {
+		pathBuilder.Reset()
+		if hasPrefix {
+			pathBuilder.WriteString(prefix)
+			pathBuilder.WriteByte('.')
+		}
+		pathBuilder.WriteString(fieldName)
+		return pathBuilder.String()
+	}
+
+	if m.IsDirty(LevelUpMechanismDirtyCurLevelBit) {
+		builder.Set(buildPath("cur_level"), m.GetCurLevel())
+	}
+	if m.IsDirty(LevelUpMechanismDirtyCurExpBit) {
+		builder.Set(buildPath("cur_exp"), m.GetCurExp())
+	}
+	if m.IsDirty(LevelUpMechanismDirtyConfIdBit) {
+		builder.Set(buildPath("conf_id"), m.GetConfId())
+	}
+}
+
+func (m *LevelUpMechanismWrapper) DeepCopy(copy *LevelUpMechanism) {
+	if m == nil || m.data == nil || copy == nil {
+		return
+	}
+
+	// 拷贝基础类型字段
+	*copy = *m.data
+}
+
+// MatchesAll 判断字段是否匹配所有类型标记
+func (m *LevelUpMechanismWrapper) MatchesAll(fieldID uint8, fieldTypes ...fieldmeta.FieldType) bool {
+	return m.fieldMetas.MatchesAll(fieldID, fieldTypes...)
+}
+
+// ToProto 将 LevelUpMechanism 数据转换为完整的 protobuf 结构体
+func (m *LevelUpMechanismWrapper) ToProto() *mme.LevelUpMechanism {
+	if m == nil || m.data == nil {
+		return nil
+	}
+
+	pb := &mme.LevelUpMechanism{}
+
+	// 设置所有字段
+	curLevel := m.data.CurLevel
+	pb.CurLevel = &curLevel
+
+	curExp := m.data.CurExp
+	pb.CurExp = &curExp
+
+	confId := m.data.ConfId
+	pb.ConfId = &confId
+
+	return pb
+}
+
+// FromProto 从 protobuf 结构体加载数据到 LevelUpMechanism
+func (m *LevelUpMechanismWrapper) FromProto(pb *mme.LevelUpMechanism) {
+	if m == nil || m.data == nil || pb == nil {
+		return
+	}
+
+	// 加载基础类型字段
+	if pb.CurLevel != nil {
+		m.SetCurLevel(*pb.CurLevel)
+	}
+
+	if pb.CurExp != nil {
+		m.SetCurExp(*pb.CurExp)
+	}
+
+	if pb.ConfId != nil {
+		m.SetConfId(*pb.ConfId)
+	}
+}
+
+// ToIncrementalProto 根据脏标记位构建增量数据的 protoMessage
+// 只返回标记为脏的字段数据，用于增量同步
+func (m *LevelUpMechanismWrapper) ToIncrementalProto(ctx mmeutils.SyncContext) proto.Message {
+	if m == nil {
+		return nil
+	}
+
+	// 如果没有脏标记，返回 nil
+	if !m.HasAnyDirty() {
+		return nil
+	}
+
+	incremental := &mme.LevelUpMechanism{}
+
+	// 根据脏标记位设置对应的字段
+	if mmeutils.FieldCanBeIncrementalSynced(m, LevelUpMechanismDirtyCurLevelBit, LevelUpMechanismFieldIndexCurLevel, ctx) {
+		v := m.GetCurLevel()
+		incremental.CurLevel = &v
+	}
+
+	if mmeutils.FieldCanBeIncrementalSynced(m, LevelUpMechanismDirtyCurExpBit, LevelUpMechanismFieldIndexCurExp, ctx) {
+		v := m.GetCurExp()
+		incremental.CurExp = &v
+	}
+
+	if mmeutils.FieldCanBeIncrementalSynced(m, LevelUpMechanismDirtyConfIdBit, LevelUpMechanismFieldIndexConfId, ctx) {
+		v := m.GetConfId()
+		incremental.ConfId = &v
+	}
+
+	return incremental
+}
