c0d4af417da16194d06e316d6af04e57c6029ec4
orbit-w <wbwwxm@gmail.com>
2025-11-09 22:58:10 +0800

    feat(service_zone): add ToProto and FromProto methods to IEntity interface for proto message handling
    - Introduced ToProto and FromProto methods in the IEntity interface to facilitate conversion to and from proto messages.
    - Enhanced the interface to support better integration with protocol buffers, improving data serialization and deserialization capabilities.

--- app/core/services/service_zone/entity.go
+++ app/core/services/service_zone/entity.go
@@ -1,10 +1,15 @@
 package servicezone
 
-import "gitee.com/orbit-w/orbit/lib/module/db/mgo_builder"
+import (
+	"gitee.com/orbit-w/orbit/lib/module/db/mgo_builder"
+	"google.golang.org/protobuf/proto"
+)
 
 type IEntity interface {
 	Name() string
 	InitFieldContext()
 	ClearAllDirtyFlags()
 	BuildMongoUpdate(builder *mgo_builder.MongoUpdateBuilder, path *mgo_builder.NestedPath)
+	ToProto() proto.Message
+	FromProto(msg proto.Message)
 }
